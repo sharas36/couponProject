cd4ca290583874d2c6d6a6d0874173135d914709
hendy-david <dhsoftwaredevelopment3012@gmail.com>
2022-04-11 11:33:10 -0400

    delete Main Checks

--- src/main/java/firstStep/Main.java
+++ src/main/java/firstStep/Main.java
@@ -10,12 +10,6 @@ public class Main {
 
 	public static void main(String[] args) throws SQLException {
 		
-		ConnectionPool connectionPool = ConnectionPool.getInstanse();
-		CompaniesDBDAO companiesDBDAO = new CompaniesDBDAO();
-		Company comp = new Company("bgfbgfbgfb", "vfbcvbcvnvnvnvbvb", "fsvfd");
-
-		companiesDBDAO.updateCompany(comp, "vfbcvbcvnvnvnvbvb", "1234");
-		System.out.println(companiesDBDAO.getCompanyIdByEmail("vfbcvbcvnvnvnvbvb"));
 
 	}
 

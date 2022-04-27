981300774b42d5dd89178a3538191151cc4cf767
hendy-david <dhsoftwaredevelopment3012@gmail.com>
2022-04-27 06:39:45 -0400

    i add testClass to all dbo- class in test file;

---
+++ src/test/java/DAO/DAOCoupons/CouponsDBDAOTest.java
@@ -0,0 +1,53 @@
+package DAO.DAOCoupons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CouponsDBDAOTest {
+    CouponsDBDAO couponsDBDAO = new CouponsDBDAO();
+
+    @Test
+    void addCoupon() {
+    }
+
+    @Test
+    void updateCouponPrice() {
+    }
+
+    @Test
+    void deleteCoupon() {
+    }
+
+    @Test
+    void getAllCoupons() {
+    }
+
+    @Test
+    void deleteAllCouponsByCompany() {
+    }
+
+    @Test
+    void getOneCoupon() {
+    }
+
+    @Test
+    void addCouponPurchase() {
+    }
+
+    @Test
+    void deleteCouponPurchase() {
+    }
+
+    @Test
+    void isThisCouponExist() {
+    }
+
+    @Test
+    void getCouponIdByCouponName() {
+    }
+
+    @Test
+    void getLock() {
+    }
+}
---
+++ src/test/java/DAO/DAOCustomers/CustomersDBDAOTest.java
@@ -0,0 +1,54 @@
+package DAO.DAOCustomers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomersDBDAOTest {
+
+    CustomersDBDAO customersDBDAO = new CustomersDBDAO();
+
+    @Test
+    void isCustomerExist() {
+    }
+
+    @Test
+    void isThisMailExist() {
+    }
+
+    @Test
+    void addCustomer() {
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+
+    @Test
+    void updateCustomerMail() {
+    }
+
+    @Test
+    void updateCustomerFirstName() {
+    }
+
+    @Test
+    void updateCustomerLastName() {
+    }
+
+    @Test
+    void updateCustomerPassword() {
+    }
+
+    @Test
+    void getCustomerIdByMail() {
+    }
+
+    @Test
+    void getAllCustomers() {
+    }
+
+    @Test
+    void getCustomer() {
+    }
+}

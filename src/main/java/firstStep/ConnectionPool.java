cce962ce8aabcb01308d3522e41b8764cf47bb73
hendy-david <dhsoftwaredevelopment3012@gmail.com>
2022-04-27 06:49:28 -0400

    in the ConnectionPool i change to Stack instead of ArrayList.  is working fine.

--- src/main/java/firstStep/ConnectionPool.java
+++ src/main/java/firstStep/ConnectionPool.java
@@ -8,64 +8,68 @@ import java.util.List;
 import java.util.Stack;
 
 public class ConnectionPool {
-	private static ConnectionPool instanse = new ConnectionPool();
+    private static ConnectionPool instanse = new ConnectionPool();
 
-	private static final String DOMAIN_STRING = "localhost";// 127.0.0.1
-	private static final String DB_NAME = "couponsproject";
-	private static final String USER_NAME = "student";
-	private static final String PASSWORD = "student";
-	private static final String CONNECTION_STRING = "jdbc:mysql://" + DOMAIN_STRING + "/" + DB_NAME + "?user="
-			+ USER_NAME + "&password=" + PASSWORD;
-	private List<Connection> connectionPool = new ArrayList<Connection>();
-	private List<Connection> connectionsInUse = new ArrayList<Connection>();
-	private Stack<Connection> stack = new Stack<>();
+    private static final String DOMAIN_STRING = "localhost";// 127.0.0.1
+    private static final String DB_NAME = "couponsproject";
+    private static final String USER_NAME = "student";
+    private static final String PASSWORD = "student";
+    private static final String CONNECTION_STRING = "jdbc:mysql://" + DOMAIN_STRING + "/" + DB_NAME + "?user="
+            + USER_NAME + "&password=" + PASSWORD;
+    //	private List<Connection> connectionPool = new ArrayList<Connection>();
+//	private List<Connection> connectionsInUse = new ArrayList<Connection>();
+    private Stack<Connection> connectionPool = new Stack<>();
 
-	// CONNECTION_STRING:
-	// jdbc:mysql://localhost:3306/example/user=root&password=1234
+    // CONNECTION_STRING:
+    // jdbc:mysql://localhost:3306/example/user=root&password=1234
 
-	private ConnectionPool() {
-		super();
-		try {
-			init();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			System.out.println("Failed to init DatabaseManager: " + e);
-		}
-	}
+    private ConnectionPool() {
+        super();
+        try {
+            init();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Failed to init DatabaseManager: " + e);
+        }
+    }
 
-	public static ConnectionPool getInstanse() {
-		return instanse;
-	}
+    public static ConnectionPool getInstanse() {
+        return instanse;
+    }
 
-	private void init() throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		System.out.println("JDBC driver loaded successfully!");
-		System.out.println("CONNECTION_STRING: " + CONNECTION_STRING);
-		for (int i = 0; i < 5; i++) {
-			Connection connection = DriverManager.getConnection(CONNECTION_STRING);
-			this.connectionPool.add(connection);
-		}
-		System.out.println("Connected to database " + DB_NAME);
-		System.out.println("We have " + connectionPool.size() + " connections");
-	}
+    private void init() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        System.out.println("JDBC driver loaded successfully!");
+        System.out.println("CONNECTION_STRING: " + CONNECTION_STRING);
+        for (int i = 0; i < 5; i++) {
+            Connection connection = DriverManager.getConnection(CONNECTION_STRING);
+            this.connectionPool.push(connection);
+        }
+        System.out.println("Connected to database " + DB_NAME);
+        System.out.println("We have " + connectionPool.size() + " connections");
+    }
 
-	public synchronized Connection getConnection() {
-		Connection connection = connectionPool.get(connectionPool.size() - 1);
+    public synchronized Connection getConnection() {
+        //Connection connection = connectionPool.get(connectionPool.size() - 1);
 
-		connectionPool.remove(connectionPool.size() - 1);
-		connectionsInUse.add(connection);
-		return connection;
-	}
 
-	public synchronized boolean restoreConnection(Connection connection) {
-		connectionPool.add(connection);
-		return connectionsInUse.remove(connection);
-	}
+//		connectionPool.remove(connectionPool.size() - 1);
+//		connectionsInUse.add(connection);
 
-	public void closeAllConnections() throws SQLException { ////check all connections not in use
-		for (Connection connection : connectionPool) {
-			connection.close();
-		}
-	}
+        return connectionPool.pop();
+    }
+
+    public synchronized boolean restoreConnection(Connection connection) {
+
+        return connectionPool.add(connection);
+
+        //return connectionsInUse.remove(connection);
+    }
+
+    public void closeAllConnections() throws SQLException { ////check all connections not in use
+        for (Connection connection : connectionPool) {
+            connection.close();
+        }
+    }
 }

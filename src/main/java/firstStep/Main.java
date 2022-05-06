05bd4186609fb643317c9c1dd4b0bd1c8031855e
hendy-david <dhsoftwaredevelopment3012@gmail.com>
2022-05-06 04:21:19 -0400

    friday 5/6/22 deleted comment

--- src/main/java/firstStep/Main.java
+++ src/main/java/firstStep/Main.java
@@ -29,7 +29,7 @@ public class Main {
         MainFacade mainFacade = userCheckScreen();
         User user = login(mainFacade);
         if (user instanceof Admin) {
-            adminMenu();
+            adminMenu(adminFacade);
         } else if (user instanceof Company) {
             companyMenu();
         } else {

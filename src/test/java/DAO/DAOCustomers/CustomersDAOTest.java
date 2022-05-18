534e6d9c3ee254f542b8af9c1ec6fec525864faa
sharas36 <apwalla36@gmail.com>
2022-05-18 20:42:53 +0300

    start facadeTests

--- src/test/java/DAO/DAOCustomers/CustomersDAOTest.java
+++ src/test/java/DAO/DAOCustomers/CustomersDAOTest.java
@@ -43,7 +43,7 @@ class CustomersDAOTest {
 
 
     }
-    //vdfvdfvdfvdf
+//dfgdfgdgdfgdfg
     @Test
     void getAllCustomers() throws SQLException {
         List<Customer> customerList = customersDBDAO.getAllCustomers();
